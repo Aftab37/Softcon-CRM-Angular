f4894e2bd07441ce8d08b7cbf5c3b286b3d4ae34
Aftab <aftabansari37786@gmail.com>
2024-01-09 12:34:29 +0530

    ASP.NET Web Api file added
    ASP.NET Web API file added

---
+++ SoftconCRM API/SoftconCRM Api/Controllers/SoftconClientDataController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SoftconCRM_Api.Data;
+
+namespace SoftconCRM_Api.Controllers
+{
+
+    #region getallclientdata
+    /// <summary>
+    /// For client data list -
+    /// Aftab Ansari - 6/1/24 Time : 5:30pm
+    /// </summary>
+
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class SoftconClientDataController : Controller
+    {
+        private readonly SoftconClientDataDbContext _SoftconClientDataDbContext;
+
+        public SoftconClientDataController(SoftconClientDataDbContext softconClientDataDbContext)
+        {
+            _SoftconClientDataDbContext = softconClientDataDbContext;
+        }   
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllClientData()
+        {
+            var ClientData = await _SoftconClientDataDbContext.SoftconClientsData.Take(10).ToListAsync();
+            return Ok(ClientData);
+        }
+
+    }
+}
+#endregion
+
